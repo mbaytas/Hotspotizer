8f211b929168b28000c0c0ae5ae9e7c16baa66f2
George Birbilis <GitHub1@zoomicon.com>
2015-08-09 16:08:13 +0300

    Small refactoring, code commenting/cleanup

--- WpfApplication/MainWindow.EditorUtils.cs
+++ WpfApplication/MainWindow.EditorUtils.cs
@@ -1,6 +1,6 @@
 ﻿//Project: Hotspotizer (https://github.com/mbaytas/hotspotizer)
 //File: MainWindow.EditorUtils.cs
-//Version: 20150731
+//Version: 20150809
 
 using HelixToolkit.Wpf;
 using System;
@@ -53,10 +53,10 @@ namespace WpfApplication
       // Initialize variable to store the initial state of the Gesture.Command being set
       CommandBackup = new ObservableCollection<Key>();
 
-      EnableKinect();
+      EnableKinect_Editor();
 
       ShowEditor();
-      ManagerOverlay.Visibility = Visibility.Visible; // Hide Manager
+
       FramesListBox.SelectedIndex = 0; // Reset Frame selection
 
       // Kill keyboard control on 3D grid
@@ -65,13 +65,13 @@ namespace WpfApplication
 
     private void KillEditor()
     {
-      DisableKinect();
-      TheWorkspace.DataContext = InitGesture; // Clean up DataContext
+      DisableKinect_Editor();
+      TheWorkspace.DataContext = InitGesture;        // Clean up DataContext
       EditorOverlay.Visibility = Visibility.Visible; // Hide Editor
       ManagerOverlay.Visibility = Visibility.Hidden; // Show Manager
     }
 
-    private void EnableKinect()
+    private void EnableKinect_Editor()
     {
       if (kinect != null)
       {
@@ -86,7 +86,7 @@ namespace WpfApplication
       }
     }
 
-    private void DisableKinect()
+    private void DisableKinect_Editor()
     {
       if (kinect != null)
       {
@@ -104,6 +104,7 @@ namespace WpfApplication
     {
       TheEditor.Visibility = Visibility.Visible;
       EditorOverlay.Visibility = Visibility.Hidden;
+      ManagerOverlay.Visibility = Visibility.Visible; // Hide Manager
     }
 
     // Enable/disable rows on SideViewGrid according to selection on FrontViewGrid
@@ -117,10 +118,10 @@ namespace WpfApplication
       {
         IList frontViewGrid_selectedCells = (IList)FVGrid.SelectedItems;
         List<int> frontViewGrid_selectedRows = new List<int>();
+
         foreach (GestureFrameCell c in frontViewGrid_selectedCells)
-        {
           frontViewGrid_selectedRows.Add((int)(c.IndexInFrame / 20));
-        }
+
         for (int i = 0; i < 400; i++)
         {
           ListBoxItem sideViewGridItemContainer = (ListBoxItem)SVGrid.ItemContainerGenerator.ContainerFromIndex(i);
@@ -132,6 +133,7 @@ namespace WpfApplication
             f.SideCells[i].IsHotspot = false;
           }
         }
+
       }
     }
 
@@ -152,6 +154,7 @@ namespace WpfApplication
           Border b = (Border)FVHints.Children[c.IndexInFrame];
           b.Background = new SolidColorBrush() { Color = Colors.SlateBlue, Opacity = 0.2 };
         }
+
         foreach (GestureFrameCell c in f.SideCells.Where(c => c.IsHotspot))
         {
           Border b = (Border)SVHints.Children[c.IndexInFrame];
@@ -174,11 +177,12 @@ namespace WpfApplication
           Opacity = 0.1 + ((double)(g.Frames.IndexOf(f) + 1) / (double)g.Frames.Count) * 0.8
         };
         DiffuseMaterial material = new DiffuseMaterial(materialBrush);
+
         foreach (GestureFrameCell fc in f.FrontCells.Where(fc => fc.IsHotspot == true))
         {
           int fcIndex = Array.IndexOf(f.FrontCells, fc);
           foreach (GestureFrameCell sc in f.SideCells.Where(
-              sc => sc.IsHotspot == true && (int)(Array.IndexOf(f.SideCells, sc) / 20) == (int)(fcIndex / 20)))
+                   sc => sc.IsHotspot == true && (int)(Array.IndexOf(f.SideCells, sc) / 20) == (int)(fcIndex / 20)))
           {
             // Init mesh
             MeshBuilder meshBuilder = new MeshBuilder(false, false);
@@ -188,8 +192,10 @@ namespace WpfApplication
             double x = (sc.LeftCM + sc.RightCM) / 2;
             Point3D cubeCenter = new Point3D(x, y, z);
             meshBuilder.AddBox(cubeCenter, 15, 15, 15);
+            
             // Create and freeze mesh
             var mesh = meshBuilder.ToMesh(true);
+            
             // Create model
             modelGroup.Children.Add(new GeometryModel3D(mesh, material));
           }
@@ -208,6 +214,7 @@ namespace WpfApplication
             Opacity = ((double)(gg.Frames.IndexOf(f) + 1) / (double)gg.Frames.Count) * 0.09
           };
           DiffuseMaterial material = new DiffuseMaterial(materialBrush);
+
           foreach (GestureFrameCell fc in f.FrontCells.Where(fc => fc.IsHotspot == true))
           {
             int fcIndex = Array.IndexOf(f.FrontCells, fc);
@@ -216,14 +223,17 @@ namespace WpfApplication
             {
               // Init mesh
               MeshBuilder meshBuilder = new MeshBuilder(false, false);
+              
               // Make cube and add to mesh
               double y = (fc.LeftCM + fc.RightCM) / 2;
               double z = (fc.TopCM + fc.BottomCM) / 2;
               double x = (sc.LeftCM + sc.RightCM) / 2;
               Point3D cubeCenter = new Point3D(x, y, z);
               meshBuilder.AddBox(cubeCenter, 15, 15, 15);
+              
               // Create and freeze mesh
               var mesh = meshBuilder.ToMesh(true);
+              
               // Create model
               modelGroup.Children.Add(new GeometryModel3D(mesh, material));
             }
@@ -256,9 +266,11 @@ namespace WpfApplication
       // Put the overlay on Editor
       EditorSetCommandOverlayKeyText.DataContext = TheWorkspace.DataContext;
       EditorSetCommandOverlay.Visibility = Visibility.Visible;
+
       // Save the current command in case of cancel
       Gesture g = (Gesture)TheWorkspace.DataContext;
       CommandBackup = new ObservableCollection<Key>(g.Command);
+
       // Listen for key press
       AddHandler(Keyboard.PreviewKeyDownEvent, (KeyEventHandler)SetCommand_KeyDown);
       AddHandler(Keyboard.PreviewKeyUpEvent, (KeyEventHandler)SetCommand_KeyUp);
@@ -266,11 +278,15 @@ namespace WpfApplication
 
     private void SetCommand_KeyDown(object sender, KeyEventArgs e)
     {
-      Gesture g = (Gesture)TheWorkspace.DataContext;
       Key k = (e.Key == Key.System ? e.SystemKey : e.Key);
-      if (KeysBeingPressed.Contains(k)) return;
-      else KeysBeingPressed.Add(k);
+      if (KeysBeingPressed.Contains(k))
+        return;
+      else
+        KeysBeingPressed.Add(k);
+
+      Gesture g = (Gesture)TheWorkspace.DataContext;
       g.Command = new ObservableCollection<Key>(KeysBeingPressed);
+
       e.Handled = true;
     }
 
@@ -278,15 +294,14 @@ namespace WpfApplication
     {
       Key k = (e.Key == Key.System ? e.SystemKey : e.Key);
       KeysBeingPressed.Remove(k);
+
       e.Handled = true;
     }
 
     private void SetCommandCancelButton_Click(object sender, RoutedEventArgs e)
     {
-      // Clean up
-      RemoveHandler(Keyboard.PreviewKeyDownEvent, (KeyEventHandler)SetCommand_KeyDown);
-      RemoveHandler(Keyboard.PreviewKeyUpEvent, (KeyEventHandler)SetCommand_KeyUp);
-      EditorSetCommandOverlay.Visibility = Visibility.Hidden;
+      Cleanup_Editor();
+
       // De-update gesture
       Gesture g = (Gesture)TheWorkspace.DataContext;
       g.Command = new ObservableCollection<Key>(CommandBackup);
@@ -294,7 +309,14 @@ namespace WpfApplication
 
     private void SetCommandOKButton_Click(object sender, RoutedEventArgs e)
     {
-      // Clean up
+      Cleanup_Editor();
+    }
+
+    /// <summary>
+    /// Clean up
+    /// </summary>
+    private void Cleanup_Editor()
+    {
       RemoveHandler(Keyboard.PreviewKeyDownEvent, (KeyEventHandler)SetCommand_KeyDown);
       RemoveHandler(Keyboard.PreviewKeyUpEvent, (KeyEventHandler)SetCommand_KeyUp);
       EditorSetCommandOverlay.Visibility = Visibility.Hidden;
@@ -303,45 +325,61 @@ namespace WpfApplication
     private void AddNewFrameButton_Click(object sender, RoutedEventArgs e)
     {
       Gesture g = (Gesture)TheWorkspace.DataContext;
-      // Add the frame to the Gesture object
-      g.Frames.Add(MakeNewGestureFrame());
-      // Extend CollisionTimes to keep track of collisions
-      CollisionTimes[GestureCollection.IndexOf(g)].Add(new DateTime());
-      // Select the newly added frame on the timeline
-      FramesListBox.SelectedIndex = FramesListBox.Items.Count - 1;
+
+      g.Frames.Add(MakeNewGestureFrame());                              // Add the frame to the Gesture object
+      CollisionTimes[GestureCollection.IndexOf(g)].Add(new DateTime()); // Extend CollisionTimes to keep track of collisions
+      FramesListBox.SelectedIndex = FramesListBox.Items.Count - 1;      // Select the newly added frame on the timeline
+
       SyncEditorGrids();
     }
 
     private void DeleteFrameButton_Click(object sender, RoutedEventArgs e)
     {
+      Gesture g = (Gesture)TheWorkspace.DataContext;
+
       Button b = (Button)sender;
       GestureFrame s = (GestureFrame)b.DataContext;
-      Gesture g = (Gesture)TheWorkspace.DataContext;
+
       g.Frames.Remove(s);
-      if (g.Frames.Count == 0) g.Frames.Add(MakeNewGestureFrame()); // Do not let a gesture have 0 frames
-      if (FramesListBox.SelectedItem == null) FramesListBox.SelectedIndex = FramesListBox.Items.Count - 1;
+
+      if (g.Frames.Count == 0)
+        g.Frames.Add(MakeNewGestureFrame()); // Do not let a gesture have 0 frames //TODO: having 0-framed gestures could be useful for creating named libraries of keyboard shortcuts as templates (say one library for PowerPoint, one for Media Player etc.) to which a user can then fill-in the gesture frames (would need the Visualizer to be aware of that and also maybe show the respective gestures colored as red - to signify they're not yet complete)
+
+      if (FramesListBox.SelectedItem == null)
+        FramesListBox.SelectedIndex = FramesListBox.Items.Count - 1;
+
       SyncEditorGrids();
     }
 
     private void MoveFrameForwardButton_Click(object sender, RoutedEventArgs e)
     {
+      Gesture g = (Gesture)TheWorkspace.DataContext;
+
       Button b = (Button)sender;
       GestureFrame f = (GestureFrame)b.DataContext;
-      Gesture g = (Gesture)TheWorkspace.DataContext;
+
       int fIndex = g.Frames.IndexOf(f);
-      if (fIndex == g.Frames.Count - 1) g.Frames.Move(fIndex, 0);
-      else g.Frames.Move(fIndex, fIndex + 1);
+      if (fIndex == g.Frames.Count - 1)
+        g.Frames.Move(fIndex, 0);
+      else
+        g.Frames.Move(fIndex, fIndex + 1);
+
       SyncEditorGrids();
     }
 
     private void MoveFrameBackwardButton_Click(object sender, RoutedEventArgs e)
     {
+      Gesture g = (Gesture)TheWorkspace.DataContext;
+
       Button b = (Button)sender;
       GestureFrame f = (GestureFrame)b.DataContext;
-      Gesture g = (Gesture)TheWorkspace.DataContext;
+
       int fIndex = g.Frames.IndexOf(f);
-      if (fIndex == 0) g.Frames.Move(fIndex, g.Frames.Count - 1);
-      else g.Frames.Move(fIndex, fIndex - 1);
+      if (fIndex == 0)
+        g.Frames.Move(fIndex, g.Frames.Count - 1);
+      else
+        g.Frames.Move(fIndex, fIndex - 1);
+
       SyncEditorGrids();
     }
 
