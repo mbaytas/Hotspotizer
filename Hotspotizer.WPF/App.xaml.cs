9afd1648056c2de79105da8b0f302de22f1733d7
George Birbilis <GitHub1@zoomicon.com>
2015-12-08 23:51:58 +0200

    - Reformatted App.xaml.cs - Added all members of Hotspotizer.Models package to Models.cd class diagram

--- Hotspotizer.WPF/App.xaml.cs
+++ Hotspotizer.WPF/App.xaml.cs
@@ -1,41 +1,50 @@
-﻿using Microsoft.Kinect;
+﻿//Project: Hotspotizer (https://github.com/mbaytas/hotspotizer, https://github.com/birbilis/hotspotizer)
+//File: App.xaml.cs
+//Version: 20151208
+
+using Microsoft.Kinect;
 using System;
 using System.Linq;
 using System.Windows;
 
 namespace Hotspotizer {
-  public partial class App : Application {
+  public partial class App : Application
+  {
     // http://channel9.msdn.com/coding4fun/kinect/When-deploying-Kinect-apps-ensure-the-runtime-components-and-device-are-ready
-    protected override void OnStartup(StartupEventArgs e) {
-      if (IsKinectRuntimeInstalled) {
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      if (IsKinectRuntimeInstalled)
         base.OnStartup(e);
-      }
-      else {
-        MessageBoxResult result = MessageBox.Show("Hotspotizer requires Microsoft Kinect Runtime 1.8 to run.\nClick \"OK\" to download Microsoft Kinect Runtime 1.8 from Microsoft's website.",
-            "Kinect Runtime required",
-            MessageBoxButton.OKCancel);
-        if (result == MessageBoxResult.OK) {
-          System.Diagnostics.Process.Start("http://www.microsoft.com/en-us/download/details.aspx?id=40277");
-        }
-      }
+      else if (MessageBoxResult.OK == MessageBox.Show(
+                "Hotspotizer requires Microsoft Kinect Runtime 1.8 to run.\nClick \"OK\" to download Microsoft Kinect Runtime 1.8 from Microsoft's website.",
+                "Kinect Runtime required",
+                MessageBoxButton.OKCancel)
+              )
+              System.Diagnostics.Process.Start("http://www.microsoft.com/en-us/download/details.aspx?id=40277");
     }
 
     public bool IsKinectRuntimeInstalled {
-      get {
+      get
+      {
         bool isInstalled;
-        try {
+        try
+        {
           TestForKinectTypeLoadException();
           isInstalled = true;
         }
-        catch (TypeInitializationException) {
+        catch (TypeInitializationException)
+        {
           isInstalled = false;
         }
         return isInstalled;
       }
     }
 
-    private void TestForKinectTypeLoadException() {
+    private void TestForKinectTypeLoadException()
+    {
       KinectSensor kinectCheck = KinectSensor.KinectSensors.FirstOrDefault();
     }
+
   }
 }
